ba7ba951c46122aef3b8e217586571d6a3a785b8
ozymandias547 <jungyue@gmail.com>
2013-11-09 13:12:51 -0600

    more setup files

---
+++ public/js/dieZombieEngine.js
@@ -0,0 +1,25 @@
+var dieZombieEngine = (function() {
+
+	var canvas = null;
+
+	function init(canvasID) {
+
+		canvas = document.getElementById(canvasID);
+		context = canvas.getContext('2d');
+
+
+
+		context.beginPath();
+		context.rect(0, 0, canvas.width, canvas.height);
+		context.fillStyle = 'yellow';
+		context.fill();
+		context.lineWidth = 7;
+		context.strokeStyle = 'black';
+		context.stroke();
+	}
+
+	return {
+		init: init
+	}
+
+})();
